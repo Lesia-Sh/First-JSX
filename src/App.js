3aa462ae3dd095b9c097ce4dd1634999c56a8d99
Lesia-Sh <107194070+Lesia-Sh@users.noreply.github.com>
2022-08-11 19:32:06 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Challenge</h1>
+      <p>Migrate this HTML from index.html to the React index.js</p>
+      <a
+        className="important"
+        href="https://reactjs.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Official React Website
+      </a>
+    </div>
+  );
+}
